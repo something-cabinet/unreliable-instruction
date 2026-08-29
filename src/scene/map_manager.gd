ac7663faa0bc108243f47b1c8f12d15c0536c1ba
NKLam <khoalamvn@gmail.com>
2026-08-29 22:04:54 +0700

    Add stall and camera locking

---
+++ src/scene/map_manager.gd
@@ -0,0 +1,5 @@
+extends Node2D
+class_name MapManager
+
+func _ready() -> void:
+    GameManager.map_manager = self
--- src/ui/camera_follow.gd
+++ src/ui/camera_follow.gd
@@ -1,6 +1,10 @@
 extends Camera2D
 class_name CameraFollow
 
+
+var min_coord = Vector2(-464, -352)
+var max_coord = Vector2(560, 16)
+
 func _process(_delta: float) -> void:
 	if GameManager.player:
-		global_position = GameManager.player.global_position
+		global_position = GameManager.player.global_position.clamp(min_coord, max_coord)
